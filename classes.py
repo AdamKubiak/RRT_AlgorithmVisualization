bcc0ce9a03bd57171e6f9c9973c3cd95d0b33780
AdamKubiak <249480@student.pwr.edu.pl>
2022-05-09 22:42:44 +0200

    działające wykrywanie kolizji

--- classes.py
+++ classes.py
@@ -58,6 +58,10 @@ class Map:
         pygame.draw.circle(self.WINDOW_,RED,self.endPos_,20,1)
         self.drawObstacles()
 
+
+
+
+
 class RRT(Map):
     def __init__(self,windowSize, startPos, endPos, obstSize,obstNum) -> None:
         Map.__init__(self,windowSize, startPos, endPos, obstSize,obstNum)
@@ -69,7 +73,71 @@ class RRT(Map):
 
         self.graphPoints_.append(self.startPos_)
         self.parents_.append(0)
+
+    def nodeAdd(self, id,position):
+        self.graphPoints_.insert(id,position)
+    
+    def nodeDelete(self,id):
+        self.graphPoints_.pop(id)
+
+    def nodeDistance(self,id1,id2) -> float:
+        X = float(self.graphPoints_[id1][0] - self.graphPoints_[id2][0])
+        Y = float(self.graphPoints_[id1][1] - self.graphPoints_[id2][1])
+
+        return np.sqrt(np.power(X)+np.power(Y))
+
+    def edgeAdd(self,parentId,childId):
+        self.parents_.insert(childId,parentId)
+    
+    def edgeRemove(self,id):
+        self.parents_.pop(id)
+
+    def randomDirectionPoint(self)->int:
+        x = int(random.uniform(0,self.winWidth_))
+        y = int(random.uniform(0,self.winHight_))
+
+        return x,y
+
+    def nodeCollisionDetection(self)->bool:
+        id = len(self.graphPoints_) - 1
+
+        for rec in self.obstaclesList_:
+            if rec.collidepoint(self.graphPoints_[id]):
+                self.nodeDelete(id)
+                return False
+            
+        return True
+            
+    def edgeCollisionDetection(self,nodeParent,nodeChild)->bool:
         
+        for rec in self.obstaclesList_:
+            for i in range(0,101):
+                u = i/100
+                x = nodeParent[0]*u + nodeChild[0]*(1-u)
+                y = nodeParent[1]*u + nodeChild[1]*(1-u)
+
+                if rec.collidepoint((x,y)):
+                    return False
+        return True
+
+    def nodeConnection(self,parentId,childId)->bool:
+        if self.edgeCollisionDetection(self.graphPoints_[parentId],self.graphPoints_[childId]):
+            self.edgeAdd(parentId,childId)
+            return True
+
+        else:
+            self.nodeDelete(childId)
+            return False
+
+
+
+
+
+
+
+
+    
+
         
 
 
@@ -78,9 +146,20 @@ def main():
     rrt = RRT((1000,600),(50,50),(510,510),30,100)
     print(rrt.graphPoints_)
     rrt.drawScene()
+    while(True):
+        x,y = rrt.randomDirectionPoint()
+        id = len(rrt.graphPoints_)
+        rrt.nodeAdd(len(rrt.graphPoints_),(x,y))
+        rrt.edgeAdd(id-1,id)
+        if rrt.nodeCollisionDetection():
+            pygame.draw.circle(rrt.WINDOW_,BLUE,rrt.graphPoints_[id],2,0)
+            if rrt.edgeCollisionDetection(rrt.graphPoints_[id-1],rrt.graphPoints_[id]):
+                pygame.draw.line(rrt.WINDOW_,PURPLE,rrt.graphPoints_[id-1],rrt.graphPoints_[id],2)
+        pygame.display.update()
+        
     #print(len(map.obstaclesList_))
     #print(map.obstNum_)
-    pygame.display.update()
+    
     pygame.event.clear()
     pygame.event.wait(0)
 
---
+++ test.py
@@ -0,0 +1,5 @@
+list = [(1,3),(2,4)]
+
+list.insert(5,(3,2))
+
+print(list)
