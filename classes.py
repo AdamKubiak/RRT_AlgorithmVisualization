76b2e54a40a43652ebb938e8c048682ada0dcb62
AdamKubiak <249480@student.pwr.edu.pl>
2022-05-15 20:18:32 +0200

    jeżdżący robot commit

--- classes.py
+++ classes.py
@@ -1,3 +1,4 @@
+from re import A
 import numpy as np
 import math
 import random
@@ -32,7 +33,7 @@ class Map:
         self.WINDOW_ = pygame.display.set_mode(windowSize)
         self.WINDOW_.fill(BLACK)
         pygame.display.set_caption('RRT Vis')
-    #generowanie współrzędnych, dodanie do listy i rysowanie przeszkody
+        
     def createRandomObstacles(self) -> None:
 
         for i in range(self.obstNum_):
@@ -69,7 +70,7 @@ class RRT(Map):
         self.graphPoints_ = []
         self.parents_ = []
         self.finalPosition_ = None
-        self.path = []
+        self.path_ = []
 
         self.graphPoints_.append(self.startPos_)
         self.parents_.append(0)
@@ -139,7 +140,7 @@ class RRT(Map):
         return nearestId
 
     #matma ;_;
-    def stepMove(self,nodeNearestId,nodeRandomId,stepSize = 7)->None:
+    def stepMove(self,nodeNearestId,nodeRandomId,stepSize = 8)->None:
         distance = self.nodeDistance(nodeNearestId,nodeRandomId)
 
         if distance > stepSize:
@@ -187,28 +188,82 @@ class RRT(Map):
     
     def drawFinalPath(self):
         positionId = len(self.graphPoints_) -1
-        while positionId >=0:
+        while positionId >0:
             pygame.draw.circle(self.WINDOW_,RED,self.graphPoints_[positionId],2,0)
-            
+            #print(self.graphPoints_[positionId])
+            self.path_.append(self.graphPoints_[positionId])
             tempPosition = positionId
             positionId = self.parents_[positionId]
             pygame.draw.line(self.WINDOW_,RED,self.graphPoints_[tempPosition],self.graphPoints_[positionId],2)
             pygame.display.update()
         
+        pygame.draw.circle(self.WINDOW_,RED,self.startPos_,2,0)
+        self.path_.append(self.graphPoints_[0])
+        self.path_.reverse()
+        pygame.display.update()
+    
         return True
 
 
+class Robot:
+    def __init__(self,startPos,robotImg,robotWidth):
+        self.meters2pixels = 3779.52 #
+        self.x = startPos[0]
+        self.y = startPos[1]
+        self.robotWidth_ = robotWidth
+        self.theta_ = 0
+        self.robotRotVel_ = 0.01*self.meters2pixels #m/s
+        self.robotLinVel_ = 0.01*self.meters2pixels
+        self.a = 20
+        self.waypoint = 0
+
+        self.robotImg_ = pygame.image.load(robotImg)
+        self.rotatedImg_ = self.robotImg_
+        self.rect = self.rotatedImg_.get_rect(center = (self.x,self.y))
+    
+    def drawRobot(self,map):
+        map.blit(self.rotatedImg_,self.rect)
+
+    def nodeDistance(self,pos1,pos2) -> float:
+        X = float(pos1[0] - pos2[0])
+        Y = float(pos1[1] - pos2[1])
+
+        return np.sqrt(np.power(X,2)+np.power(Y,2))
+
+    def moveInverseRobot(self,finalPath):
+        nextPos = finalPath[self.waypoint]
+        deltaX = nextPos[0] - self.x
+        deltaY = nextPos[1] - self.y
+
+        self.robotLinVel_ = deltaX*math.cos(self.theta_) + deltaY*math.sin(self.theta_)
+        self.robotRotVel_ = (-1/self.a)*math.sin(self.theta_) * deltaX + (1/self.a)*math.cos(self.theta_)*deltaY
+
+        # jesli dystans miedzy pozycją robota a pozycją nextpoint jest mniejszy o ileś to iteruj waypoint
+        if self.nodeDistance((self.x,self.y),finalPath[self.waypoint]) < 35:
+            self.waypoint +=1
+        
+        if self.waypoint > (len(finalPath)-1):
+            self.waypoint = len(finalPath)-1
+
+    def moveRobot(self,dt,finalPath):
+        self.x += (self.robotLinVel_*math.cos(self.theta_) - self.a*math.sin(self.theta_)*self.robotRotVel_)*dt
+        self.y += (self.robotLinVel_*math.sin(self.theta_)+self.a*math.cos(self.theta_)*self.robotRotVel_)*dt
+
+        self.theta_+= self.robotRotVel_*dt
+        self.rotatedImg_ = pygame.transform.rotozoom(self.robotImg_,math.degrees(-self.theta_),1)
+        self.rect = self.rotatedImg_.get_rect(center = (self.x,self.y))
+        self.moveInverseRobot(finalPath)
+        
 
 
 def main():
-    rrt = RRT((1000,600),(50,50),(510,510),30,200)
-    
+    rrt = RRT((1000,600),(50,50),(510,510),30,50)
     rrt.drawScene()
+    robot = Robot(rrt.startPos_,r"D:\GitRepos\RRT_AlgorithmVisualization\robot1.png",80)
     iteration = 0     
         
     while (rrt.isFinished_ == False):
-        #id = len(rrt.graphPoints_)
-        if iteration % 5 == 0 and rrt.isFinished_ == False:
+        if iteration % 10 == 0 and rrt.isFinished_ == False:
             rrt.moveToEndPos(rrt.endPos_)
             pygame.draw.circle(rrt.WINDOW_,BLUE,rrt.graphPoints_[-1],2,0)
             pygame.draw.line(rrt.WINDOW_,PURPLE,rrt.graphPoints_[-1],rrt.graphPoints_[rrt.parents_[-1]],2)
@@ -220,13 +275,29 @@ def main():
             pygame.draw.line(rrt.WINDOW_,PURPLE,rrt.graphPoints_[-1],rrt.graphPoints_[rrt.parents_[-1]],2)
 
         iteration+=1
-
+       # robot.drawRobot(rrt.WINDOW_)
         pygame.display.update()
-
-        
     rrt.drawFinalPath()
-    print(len(rrt.graphPoints_))
-    print(len(rrt.parents_))
+    
+    dt = 0
+    lasttime = pygame.time.get_ticks()
+    run = 1
+    check = False
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = 0
+        
+        #rrt.WINDOW_.fill(BLACK)
+        robot.moveRobot(dt,rrt.path_)
+        robot.drawRobot(rrt.WINDOW_)
+        
+        check = rrt.drawFinalPath()
+        
+        
+        pygame.display.update()
+        dt = (pygame.time.get_ticks()-lasttime)/1000
+        lasttime = pygame.time.get_ticks()
 
     pygame.event.clear()
     pygame.event.wait(0)
--- test.py
+++ test.py
@@ -1,5 +1,41 @@
-list = [(1,3),(2,4)]
+import re
+with open("twin.txt",encoding="utf-8") as file_in:
+    lines = []
+    for line in file_in:
+        lines.append(line)
+i = 6
+newdoclist= []
+print(lines[i])
+print(lines[i][6])
+for line in lines:
+    if re.search('00:',line):
+        dziesiatki = int(float(line[6]))*10
+        jednosci = int(float(line[7]))
+        dziesiatki1 = int(float(line[23]))*10
+        jednosci1 = int(float(line[24]))
+        wynik = str(dziesiatki+jednosci+5)
+        wynik1 = str(dziesiatki1+jednosci1+5)
+        if int(wynik) < 10:
+            wynik = '0'+wynik
+        
+        if int(wynik1) < 10:
+            wynik1 = '0'+wynik1
+        if int(wynik) > 60:
+            wynik = '60'
 
-list.insert(5,(3,2))
-for i in range(0,5):
-    print(i)
+        if int(wynik1) > 60:
+            wynik1 = '60'
+        przod = line[:6]
+        srodek = line[8:23]
+        tyl = line[25:]
+        NEWLINE = przod+wynik+srodek+wynik1+tyl
+        newdoclist.append(NEWLINE)
+    else: 
+        newdoclist.append(line)
+
+
+with open('your_file.txt', 'w',encoding="utf-8") as f:
+    for item in newdoclist:
+        f.write("%s" % item)
+
+#print(line)
